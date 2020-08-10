0a86c7638c3a4254eb1b2d8e0b4d5f57a9a05a10
PabloEzequielCampos <68036545+PabloEzequielCampos@users.noreply.github.com>
2020-08-10 19:47:48 -0300

    Add files via upload

--- uno.js
+++ uno.js
@@ -7,7 +7,67 @@ b)la edad promedio en total.
 c)el hombre mas pesado.
 pedir datos por prompt y mostrar por document.write o console.log
 */
-function mostrar()
-{
-	alert("uno");
+
+
+function mostrar() {
+
+	let nombre;
+	let peso;
+	let pesomax = 0;
+	let sexo;
+	let edad;
+	let cantidadF = 0;
+	let sumaEdad = 0;
+	let promedio;
+	let contadorEdad = 0;
+
+	for (let i = 0; i < 5; i++) {  ///// volver a 5.
+
+		do {
+			nombre = prompt("Ingrese Nombre");
+
+		} while (!(isNaN(nombre)));
+
+		do {
+			peso = parseInt(prompt("Ingrese Peso"));
+
+		} while ((isNaN(peso)));
+
+		do {
+			sexo = prompt("Ingrese sexo:  f/m");
+
+		} while (sexo != "f" && sexo != "m");
+
+		do {
+			edad = parseInt(prompt("Ingrese Edad"));
+
+		} while (isNaN(edad));
+
+		if (sexo == "f") {
+			cantidadF++;
+		}
+
+		if (edad > 0) {
+			contadorEdad++
+			sumaEdad = sumaEdad + edad;
+			promedio = sumaEdad / contadorEdad;
+		}
+		if (peso > pesomax && sexo == "m") {
+			pesomax = peso;
+
+		}
+
+	}//FINALIZA EL FOR
+
+	document.write(cantidadF + " : Es la cantidad de  Mujeres" + "</br>");
+	document.write(promedio + " :  Es la Edad promedio General" + "</br>");
+	document.write("El hombre mas PESADO es:  " + pesomax + "</br>");
+
 }
+
+
+
+
+
+
+
