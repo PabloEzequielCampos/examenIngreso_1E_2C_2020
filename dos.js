8a2fa60efdb3ea0008c6f72ce331a66814bad50b
PabloEzequielCampos <68036545+PabloEzequielCampos@users.noreply.github.com>
2020-08-10 20:49:52 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -5,7 +5,54 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/
+function mostrar() {
+  let resp;
+  let marca;
+  let precio = 0;
+  let acumuladorPeso = 0;
+  let peso = 0;
+  let tipo;
+  let productoMasCaroLiquidos = 0;
+  let productoMasBaratoSolidos = 0;
+  let marcaMasCaraLiquido;
+  let marcaMasBarataSolidos;
+
+  do {
+
+    do {
+      marca = prompt("Ingrese marca");
+    } while (!(isNaN(marca)));
+
+    do {
+      precio = parseInt(prompt("Ingrese precio"));
+
+    } while ((isNaN(precio)));
+
+    do {
+      peso = parseInt(prompt("Ingrese peso"));
+      acumuladorPeso += peso;
+    } while ((isNaN(peso)));
+
+    do {
+      tipo = (prompt("Ingrese tipo : solido / liquido"));
+    } while (!(tipo == "solido" || tipo == "liquido"));
+
+
+    if (precio > productoMasCaroLiquidos && tipo == 'liquido') {
+      marcaMasCaraLiquido = marca;
+      productoMasCaroLiquidos = precio;
+    }
+
+    if (precio > productoMasBaratoSolidos && tipo == 'solido') {
+      marcaMasBarataSolidos = marca;
+      productoMasBaratoSolidos = precio;
+    }
+
+    resp = prompt(" Ingresa otro ?");
+  } while (resp == "s");
+
+  document.write("El peso total de la compra ess :" + acumuladorPeso + "<br>");
+  document.write("El mas caro de los liquidos es de marca: " + marcaMasCaraLiquido + "<br>");
+  document.write("El mas barato de los solidos es:  " + marcaMasBarataSolidos + "<br>");
 }
